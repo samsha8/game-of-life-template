09e5e94ab1d8a97ea8968ed3b5ed8be2a031b212
samsha8 <sameirasharma@gmail.com>
2025-03-03 21:49:47 +0000

    I added a method

--- GameOfLife.java
+++ GameOfLife.java
@@ -2,15 +2,47 @@ import java.util.Arrays;
 
 public class GameOfLife implements Board {
 
-    // Integers: 0 or 1 for alive or dead
     private int[][] board;
 
-    public GameOfLife(int x, int y)
-    {
-        // Construct a 2d array of the given x and y size.
+    public GameOfLife(int x, int y) {
+        board = new int[x][y];
     }
 
-    // Set values on the board
+    public void run(int turns) {
+        for (int i = 0; i < turns; i++) {
+            step();
+        }
+    }
+
+    public void step() {
+        int[][] newBoard = new int[board.length][board[0].length];
+        
+        for (int x = 0; x < board.length; x++) {
+            for (int y = 0; y < board[0].length; y++) {
+                int neighbors = countNeighbors(x, y);
+                if (board[x][y] == 1) {
+                    newBoard[x][y] = (neighbors == 2 || neighbors == 3) ? 1 : 0;
+                } else {
+                    newBoard[x][y] = (neighbors == 3) ? 1 : 0;
+                }
+            }
+        }
+        board = newBoard;
+        print();
+    }
+
+    public int countNeighbors(int x, int y) { 
+        int count = 0;
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                if (!(i == 0 && j == 0)) {
+                    count += get(x + i, y + j);
+                }
+            }
+        }
+        return count;
+    }
+    
     public void set(int x, int y, int[][] data) {
         for (int i = 0; i < data.length; i++) {
             for (int j = 0; j < data[0].length; j++) {
@@ -19,63 +51,33 @@ public class GameOfLife implements Board {
         }
     }
 
-    // Run the simulation for a number of turns
-    public void run(int turns) {
-        // call step the number of times requested
+    public int get(int x, int y) { 
+        int xLimit = board.length; 
+        int yLimit = board[0].length;
+        return board[(x + xLimit) % xLimit][(y + yLimit) % yLimit];
     }
 
-    // Step the simulation forward one turn.
-    public void step()
-    {
-        print();
-        // Update the game board, store a 1 if the cell is alive and a 0 otherwise.
-    }
-
-
-    public int countNeighbors(int x, int y) {
-        int count = 0;
-        // count the number of neighbors the cell has
-        // use the get(x,y) method to read any board state you need.
-        return count;
-    }
-
-    // Get a value from the board with "wrap around"
-    // Locations outside the board will loop back into the board.
-    // Ex: -1 will read board.length-1
-    public int get(int x, int y) {
-        int xLimit = board.length;
-        int yLimit= board[0].length;
-        return board[(x+xLimit)%xLimit][(y+yLimit)%yLimit];
-    }
-
-    // Test helper to get the whole board state
-    public int[][] get()
-    {
+    public int[][] get() {
         return board;
     }
 
-    // Test helper to print the current state
-    public void print(){
-        // Print the header
+    public void print() {
         System.out.print("\n ");
         for (int y = 0; y < board[0].length; y++) {
-            System.out.print(y%10 + " ");
+            System.out.print(y % 10 + " ");
         }
 
         for (int x = 0; x < board.length; x++) {
-            System.out.print("\n" + x%10);
-            for (int y=0; y<board[x].length; y++)
-            {
-                if (board[x][y] == 1)
-                {
+            System.out.print("\n" + x % 10);
+            for (int y = 0; y < board[x].length; y++) {
+                if (board[x][y] == 1) {
                     System.out.print("⬛");
-                }
-                else
-                {
+                } else {
                     System.out.print("⬜");
                 }
             }
         }
         System.out.println();
+
     }
 }
